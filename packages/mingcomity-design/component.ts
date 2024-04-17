f55d301e89c945826255879a410ef6293d4af303
Mingcomity <2662504913@qq.com>
2024-04-17 21:24:33 +0800

    fix(mingcomity-design): 修复引入
    修复McCollapseItem引入错误问题

--- packages/mingcomity-design/component.ts
+++ packages/mingcomity-design/component.ts
@@ -1,6 +1,5 @@
 import { McButton } from '@mingcomity-design/components/button'
-import { McCollapse } from '@mingcomity-design/components/collapse'
-import { McCollapseItem } from '@mingcomity-design/components/collapse-item'
+import { McCollapse, McCollapseItem } from '@mingcomity-design/components/collapse'
 import type { DefineComponent } from 'vue'
 
 export default [McButton, McCollapse, McCollapseItem] as unknown as DefineComponent[]
